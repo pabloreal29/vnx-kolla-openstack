4b4d51fd542de5e615b7a45751feaa3398ef3f5c
pabloreal29 <pabloreal296@gmail.com>
2024-04-21 14:52:48 +0200

    No SSH / Net2 Conectivity

--- deploy/init_escenario.sh
+++ deploy/init_escenario.sh
@@ -3,16 +3,24 @@
 #Borrar la línea que incluye la ruta /home/giros dentro de ./filesystems/rootfs_lxc_ubuntu64-ostack-controller/config
 # sudo sed -i '/lxc.mount.entry = \/home\/giros\/vnx-lab-openstack-antelope\/shared \/root\/.vnx\/scenarios\/openstack_lab-antelope\/vms\/controller\/mnt\/rootfs\/\/root\/shared none bind 0 0/d' ./filesystems/rootfs_lxc_ubuntu64-ostack-controller/config
 
-# sudo vnx -f openstack_lab.xml -v --create
-# sudo vnx -f openstack_lab.xml -x start-all,load-img
+# Almacenar las claves del nodo admin
+rm -rf $HOME/.config/openstack/
+mkdir $HOME/.config/openstack/
+scp root@admin:/etc/kolla/clouds.yaml $HOME/.config/openstack/
+openstack --os-cloud kolla-admin service list
 
 #Habilitar NAT para tener conectividad con el exterior
 source deploy/habilitar-nat.sh
 
-#Crear usuario
+#Crear mi usuario
 openstack --os-cloud kolla-admin user create myuser --project admin --password xxxx
 openstack --os-cloud kolla-admin role add admin --user myuser --project admin 
 source deploy/myuser-openrc.sh
+
+#Cargar imagenes y crear red externa
+OSTACKLAB=openstack_kolla_ansible_2023_2.xml
+sudo vnx -f $OSTACKLAB -x load-img
+sudo vnx -f $OSTACKLAB -x create-extnet
 
 #Crear parejas de claves
 rm -rf ./tmp/keys
@@ -36,6 +44,7 @@ openstack image create "focal-bbdd-vnx" --file /home/pabloreal/openstack-images/
 openstack image create "focal-admin-vnx" --file /home/pabloreal/openstack-images/admin_image.qcow2 --disk-format qcow2 --container-format bare --public --progress
 
 #Crear el flavor de la bbdd (con el m1.smaller no puede ejecutar Mongo, necesita mas capacidad)
+#Nota: he reducido las vcpus y la ram respecto al trabajo de cnvr
 openstack flavor create m1.large --vcpus 1 --ram 512 --disk 5
 
 #Crear el stack con todos los elementos del escenario
@@ -44,17 +53,17 @@ openstack stack create -t deploy/escenarioTF.yml stackTF
 # # Borrar el stack
 # openstack stack delete -y stackTF
 
-#Esperar a que se cree el router del escenario antes de asignarle el firewall 
-sleep 120
+# #Esperar a que se cree el router del escenario antes de asignarle el firewall 
+# sleep 120
 
-#Configuración del firewall
-router_port=$(openstack port list --router firewall_router --fixed-ip ip-address=10.1.1.1 -c ID -f value)
-openstack firewall group rule create --protocol tcp --destination-ip-address 10.1.1.57 --destination-port 2022 --action allow --name ssh_admin 
-openstack firewall group rule create --protocol tcp --destination-ip-address 10.1.1.23 --destination-port 80 --action allow --name www_lb 
-openstack firewall group rule create --protocol any --source-ip-address 10.1.1.0/24 --action allow --name server_connection 
-openstack firewall group policy create --firewall-rule ssh_admin --firewall-rule www_lb grupo1_policy_ingress 
-openstack firewall group policy create --firewall-rule server_connection grupo1_policy_egress
-openstack firewall group create --ingress-firewall-policy grupo1_policy_ingress --egress-firewall-policy  grupo1_policy_egress --port $router_port --name grupo1_firewall_group
+# #Configuración del firewall
+# router_port=$(openstack port list --router firewall_router --fixed-ip ip-address=10.1.1.1 -c ID -f value)
+# openstack firewall group rule create --protocol tcp --destination-ip-address 10.1.1.57 --destination-port 2022 --action allow --name ssh_admin 
+# openstack firewall group rule create --protocol tcp --destination-ip-address 10.1.1.23 --destination-port 80 --action allow --name www_lb 
+# openstack firewall group rule create --protocol any --source-ip-address 10.1.1.0/24 --action allow --name server_connection 
+# openstack firewall group policy create --firewall-rule ssh_admin --firewall-rule www_lb grupo1_policy_ingress 
+# openstack firewall group policy create --firewall-rule server_connection grupo1_policy_egress
+# openstack firewall group create --ingress-firewall-policy grupo1_policy_ingress --egress-firewall-policy  grupo1_policy_egress --port $router_port --name grupo1_firewall_group
 
 # # #Añadir servidores extra con autoescalado al escenario
 # # openstack stack create -t /home/p.realb/Desktop/CNVR/TF/autoScalingGroup.yml stackExtra
